8309c1a425f166c2cb70e4e23a4b9ad7aba000e2
Ross Hettel <rhettel@highground.com>
2014-06-22 16:56:00 -0500

    Initial Commit

---
+++ Tippr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippr
+//
+//  Created by Ross Hettel on 6/22/14.
+//  Copyright (c) 2014 Ross Hettel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
